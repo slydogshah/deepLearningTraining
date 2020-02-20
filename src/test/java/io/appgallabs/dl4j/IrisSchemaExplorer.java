b471b955e38d557e5b3110590ca487c740a29655
babyboy <bugs.bunny.shah@gmail.com>
2020-02-19 21:38:47 -0600

    iris_schema_explorer

--- src/test/java/io/appgallabs/dl4j/IrisSchemaExplorer.java
+++ src/test/java/io/appgallabs/dl4j/IrisSchemaExplorer.java
@@ -8,18 +8,21 @@ import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.transform.analysis.DataAnalysis;
 import org.datavec.api.transform.analysis.columns.DoubleAnalysis;
 import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.ui.HtmlAnalysis;
 import org.datavec.api.writable.Writable;
 import org.datavec.spark.transform.AnalyzeSpark;
 import org.datavec.spark.transform.misc.StringToWritablesFunction;
 import org.deeplearning4j.examples.download.DownloaderUtility;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.List;
 
 public class IrisSchemaExplorer {
 
     @Test
     public void testExplorer() throws Exception {
+        //Build a Schema based on CSV comma-separated values and add the labels
         Schema schema = new Schema.Builder()
                 .addColumnsDouble("Sepal length", "Sepal width", "Petal length", "Petal width")
                 .addColumnInteger("Species")
@@ -38,6 +41,7 @@ public class IrisSchemaExplorer {
         RecordReader rr = new CSVRecordReader();
         JavaRDD<List<Writable>> parsedInputData = stringData.map(new StringToWritablesFunction(rr));
 
+        //Produce an Analysis of the DataSet
         int maxHistogramBuckets = 10;
         DataAnalysis dataAnalysis = AnalyzeSpark.analyze(schema, parsedInputData, maxHistogramBuckets);
 
@@ -48,5 +52,15 @@ public class IrisSchemaExplorer {
         double minValue = da.getMin();
         double maxValue = da.getMax();
         double mean = da.getMean();
+
+        System.out.println(da);
+
+        //HTML Output of dataAnalysis
+        HtmlAnalysis.createHtmlAnalysisFile(dataAnalysis, new File("DataVecIrisAnalysis.html"));
+
+        //HTML Output of Sepal length Analysis, but same output as dataAnalysis. which is interesting
+        //considering da is just a subset of dataAnalysis.
+        //Food for thought, but moving to the next component
+        HtmlAnalysis.createHtmlAnalysisFile(dataAnalysis, new File("DataVecIrisAnalysis2.html"));
     }
 }
